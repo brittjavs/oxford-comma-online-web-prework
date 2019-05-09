bc2d79070b4f096f799ea66f258ac5855a4220d7
Brittany Javalera <brittjavalera@gmail.com>
2019-05-09 04:34:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
 if array.length == 2
   array.join(" and ")
   elsif array.length >= 3
-  array[-2] << " and"
+  new_array = array[-2] << " and"
+  new
 else array.join
 end
 end
