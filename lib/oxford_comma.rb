f2121a83a18ae1dc76fe23ace63e23aaefcb00cd
Brittany Javalera <brittjavalera@gmail.com>
2019-05-09 04:25:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array)
-if array.length == 2
-array.to_sentence
-elsif array.length == 1
-array.join
-else array.length >= 3
-  array.join(" ,") + " and " + array[-1]
+if array.length >= 2
+  array.to_sentence
+else array.join
 end
 end
